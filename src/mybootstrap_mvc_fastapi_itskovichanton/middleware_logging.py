516286ea6ec472fd3a30ba29ac6527dd8b0934de
aitskovich <a.itskovich@molbulak.com>
2023-04-30 22:13:39 +0300

    увел логирование ответа в другой поток

--- src/mybootstrap_mvc_fastapi_itskovichanton/middleware_logging.py
+++ src/mybootstrap_mvc_fastapi_itskovichanton/middleware_logging.py
@@ -1,4 +1,7 @@
+import time
 import typing
+from asyncio import new_event_loop, set_event_loop
+from concurrent.futures import ThreadPoolExecutor
 from logging import Logger
 from typing import Callable, Awaitable, Tuple, Dict, List
 
@@ -26,37 +29,46 @@ class RequestWithBody(Request):
             return {"type": "http.request", "body": self._body, "more_body": False}
 
 
+class HTTPLogLineCompiler:
+
+    async def get_log_line(self, request, req_body, response_body, response, elapsed_time_ms):
+        return {"response_headers": utils.tuple_to_dict(response.headers.items()),
+                "request_headers": utils.tuple_to_dict(request.headers.items()),
+                "method": request.method, "url": request.url, "request-body": req_body,
+                "response": response_body, "response_code": response.status_code, "elapsed_ms": elapsed_time_ms}
+
+
 class HTTPLoggingMiddleware(BaseHTTPMiddleware):
 
     def __init__(self, app: ASGIApp, logger: Logger,
                  dispatch: typing.Optional[DispatchFunction] = None,
-                 encoding: str = "utf-8"):
+                 encoding: str = "utf-8",
+                 async_mode: bool = True,
+                 log_line_compiler: HTTPLogLineCompiler = HTTPLogLineCompiler()):
         super().__init__(app, dispatch)
-        self.logger = logger
-        self.encoding = encoding
+        self._logger = logger
+        self._encoding = encoding
+        self._async_mode = async_mode
+        if not log_line_compiler:
+            log_line_compiler = HTTPLogLineCompiler()
+        self._log_line_compiler = log_line_compiler
+        if async_mode:
+            self._pool = ThreadPoolExecutor()
+        self._loop = new_event_loop()
+        set_event_loop(self._loop)
 
     async def dispatch(self, request: Request,
                        call_next: Callable[[Request], Awaitable[StreamingResponse]]) -> Response:
 
+        start_time = int(round(time.time() * 1000))
         request_body_bytes = await request.body()
         request_with_body = RequestWithBody(request.scope, request_body_bytes)
 
         response = await call_next(request_with_body)
-        response_content_bytes, response_headers, response_status = await self._get_response_params(response)
 
-        try:
-            req_body = request_body_bytes.decode(self.encoding)
-        except:
-            req_body = "<request>"
+        self._log_response(request_body_bytes, response, request, int(round(time.time() * 1000) - start_time))
 
-        response_body = response_content_bytes.decode(self.encoding)
-
-        self.logger.info({"response_headers": utils.tuple_to_dict(response.headers.items()),
-                          "request_headers": utils.tuple_to_dict(request.headers.items()),
-                          "method": request.method, "url": request.url, "request-body": req_body,
-                          "response": response_body, "response_code": response.status_code})
-
-        return Response(response_content_bytes, response_status, response_headers)
+        return response
 
     async def _get_response_params(self, response: StreamingResponse) -> Tuple[bytes, Dict[str, str], int]:
         """Getting the response parameters of a response and create a new response."""
@@ -68,10 +80,31 @@ class HTTPLoggingMiddleware(BaseHTTPMiddleware):
             if message["type"] == "http.response.start":
                 response_status.append(message["status"])
                 response_headers.append(
-                    {k.decode(self.encoding): v.decode(self.encoding) for k, v in message["headers"]})
+                    {k.decode(self._encoding): v.decode(self._encoding) for k, v in message["headers"]})
             else:
                 response_byte_chunks.append(message["body"])
 
         await response.stream_response(send)
         content = b"".join(response_byte_chunks)
         return content, response_headers[0], response_status[0]
+
+    def _log_response(self, request_body_bytes, response, request, elapsed_time_ms):
+        log_future = self._async_log_response(request_body_bytes, response, request, elapsed_time_ms)
+        if self._pool:
+            return self._pool.submit(self._loop.run_until_complete, log_future)
+        self._loop.run_until_complete(log_future)
+
+    async def _async_log_response(self, request_body_bytes, response, request, elapsed_time_ms):
+        response_content_bytes, response_headers, response_status = await self._get_response_params(response)
+
+        try:
+            req_body = request_body_bytes.decode(self._encoding)
+        except:
+            req_body = "<request>"
+
+        response_body = response_content_bytes.decode(self._encoding)
+
+        log_dict = await self._log_line_compiler.get_log_line(request, req_body, response_body, response,
+                                                              elapsed_time_ms)
+        if log_dict is not None:
+            self._logger.info(log_dict)
--- tests/server.py
+++ tests/server.py
@@ -1,11 +1,43 @@
+import json
+
 import uvicorn
 from fastapi import Request, FastAPI
 from pydantic import BaseModel
 from src.mybootstrap_core_itskovichanton.logger import LoggerService
 from src.mybootstrap_ioc_itskovichanton.ioc import bean
 
-from src.mybootstrap_mvc_fastapi_itskovichanton.middleware_logging import HTTPLoggingMiddleware
 from controller import TestController, TestController2
+from src.mybootstrap_mvc_fastapi_itskovichanton import utils
+from src.mybootstrap_mvc_fastapi_itskovichanton.middleware_logging import HTTPLoggingMiddleware, HTTPLogLineCompiler
+
+
+class CompactHTTPLogLineCompiler(HTTPLogLineCompiler):
+
+    def _preprocess_param_value(self, k: str, v):
+        k = k.upper()
+        if "BLOB" in k or "BASE64" in k:
+            return "<bytes>"
+        return v
+
+    async def get_log_line(self, request, req_body, response_body, response, elapsed_time_ms):
+        session_token = utils.tuple_to_dict(request.headers.items()).get("sessionToken")
+        request_params = dict(request.query_params)
+        if request.method == "POST":
+            try:
+                f = await request.form()
+                request_params.update(f.items())
+            except:
+                ...
+        request_params = {k: self._preprocess_param_value(k, v) for k, v in request_params.items()}
+        if "json" in response.headers["content-type"]:
+            r = json.loads(response_body)
+            if type(r) == dict:
+                r = {k: self._preprocess_param_value(k, v) for k, v in r.items()}
+                response_body = json.dumps(r)
+        return {
+            "sessionToken": session_token,
+            "method": request.method, "url": request.url, "request-params": request_params,
+            "response": response_body, "response_code": response.status_code}
 
 
 @bean(port=("server.port", int, 8000))
@@ -17,6 +49,7 @@ class TestServer:
     def start(self):
         fast_api = FastAPI(title='Test', debug=False)
         fast_api.add_middleware(HTTPLoggingMiddleware,
+                                # log_line_compiler=CompactHTTPLogLineCompiler(),
                                 encoding="utf-8",
                                 logger=self.logger_service.get_file_logger("http"))
 
@@ -25,8 +58,15 @@ class TestServer:
             return await self.test_controller.test(table, request, q, limit, count)
 
         @fast_api.post("/search10/{table}")
-        async def m1(table: str, request: Request, p: SearchParams):
-            return {"name": "anton"}
+        async def m1(table: str, request: Request):
+            params = dict(request.query_params)
+            if request.method == "POST":
+                try:
+                    f = await request.form()
+                    params.update(f.items())
+                except:
+                    ...
+            return params
 
         @fast_api.post("/search3/{table}")
         async def m1(table: str, request: Request, p: SearchParams):
